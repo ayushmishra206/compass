3580bec0ddbd3fd92b007b597b543c071bfd74cf
Ayush Mishra <ayushmishra206@gmail.com>
2026-05-09 21:40:38 +0530

    feat(core): export new credentials + passphrase helpers from package barrel

--- packages/core/src/crypto/index.ts
+++ packages/core/src/crypto/index.ts
@@ -1,2 +1,14 @@
-export * from './keystore';
-export * from './credentials';
+export {
+  getActiveCredentials,
+  setActiveCredentials,
+  clearActiveCredentials,
+  enableEncryption,
+  disableEncryption,
+  unlockCredentials,
+  lockCredentials,
+  isEncryptionEnabled,
+  isLocked,
+  LlmCredentialsLocked,
+} from './credentials';
+export { encrypt, decrypt, EncryptedSecretSchema, type EncryptedSecret } from './keystore';
+export { MIN_PASSPHRASE_LENGTH, passphraseStrength, passphraseError } from './passphrase';
