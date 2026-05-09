b732f567c341ac2c7cc1a055b860f6ac62556974
Ayush Mishra <ayushmishra206@gmail.com>
2026-05-09 12:03:05 +0530

    refactor(ui): delete layout primitives (AppShell, Sidebar, Topbar, Surface, Grid)

--- packages/ui/src/layout/index.ts
+++
@@ -1,5 +0,0 @@
-export { AppShell, type AppShellProps } from './AppShell.js';
-export { Sidebar, type SidebarProps } from './Sidebar.js';
-export { Topbar, type TopbarProps } from './Topbar.js';
-export { Surface } from './Surface.js';
-export { Grid12 } from './Grid.js';
