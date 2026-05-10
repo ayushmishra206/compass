bf95bbd4ed14e276395992be51ebe89ac20a8196
Ayush Mishra <ayushmishra206@gmail.com>
2026-05-10 11:18:29 +0530

    feat(core): export profile module from package barrel

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -5,3 +5,4 @@ export * from './scenes/types.js';
 export { hashSeed } from './scenes/hash.js';
 export { codeToAffinity } from './scenes/codeToAffinity.js';
 export { pickMoodByHour, pickScene } from './scenes/picker.js';
+export * from './profile/index.js';
